c60ebfe837391545f8b88a73bb4fdde1457788d0
cramallo <cramallo65@gmail.com>
2019-09-02 17:34:02 -0300

    adding address and contact library info

--- src/main/java/com/mock/librarytheones/dtos/BookApiDTO.java
+++ src/main/java/com/mock/librarytheones/dtos/BookApiDTO.java
@@ -12,6 +12,10 @@ public class BookApiDTO {
     private int numberOfPages;
     private List<String> authors = new ArrayList<String>();
     private float price;
+    private String libraryName = "Library One";
+    private String address = "Corrientes 348, Ciudad Autonoma de Buenos Aires";
+    private String phone =  "42499578";
+    private String email = "libraryOne@email.com";
 
     public BookApiDTO(){}
 
@@ -89,5 +93,37 @@ public class BookApiDTO {
         if (!this.publishers.contains(publisher)) {
             this.publishers.add(publisher);
         }
+    }
+
+    public String getLibraryName() {
+        return libraryName;
+    }
+
+    public void setLibraryName(String libraryName) {
+        this.libraryName = libraryName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
